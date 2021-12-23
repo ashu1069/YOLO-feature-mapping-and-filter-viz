5f3121fd7ea005a82a7885da3c8ed2bbc0898e34
Ashutosh Kumar <58232141+ashu1069@users.noreply.github.com>
2021-12-23 17:33:12 +0530

    Create YOLO_scratch.py

---
+++ YOLO_scratch.py
@@ -0,0 +1,102 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from tensorflow import keras
+from keras.models import Model
+from keras.models import Sequential
+from keras.models import  load_model
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.regularizers import l2
+
+#Rectified Layer Unit activation function and coefficient of Leaky ReLU is 0.1
+lrelu = keras.layers.LeakyReLU(alpha=0.1)
+
+#Building the YOLO architecture
+model = Sequential()
+model.add(Conv2D(filters=64, kernel_size= (7, 7), strides=(1, 1), input_shape =(448, 448, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding = 'same'))
+
+model.add(Conv2D(filters=192, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding = 'same'))
+
+model.add(Conv2D(filters=128, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=256, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=256, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=512, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding = 'same'))
+
+model.add(Conv2D(filters=256, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=512, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=256, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=512, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=256, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=512, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=256, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=512, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=512, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=1024, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding = 'same'))
+
+model.add(Conv2D(filters=512, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=1024, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=512, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=1024, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=1024, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=1024, kernel_size= (3, 3), strides=(2, 2), padding = 'same'))
+
+model.add(Conv2D(filters=1024, kernel_size= (3, 3), activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=1024, kernel_size= (3, 3), activation=lrelu, kernel_regularizer=l2(5e-4)))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Dense(1024))
+model.add(Dropout(0.5))
+model.add(Dense(1470, activation='sigmoid'))
+print(model.summary())
+model.save("model.h5")
+
+#Studying and analyzing the layers of the YOLO architecture
+layer = model.layers
+filters, biases = model.layers[0].get_weights()    #the indices of layers can be chosen once you see the summary of the model
+print(layer[0].name, filters.shape)
+
+#Plotting the filters
+fig1 = plt.figure(figsize=(8,12))
+columns=8
+rows=8
+n_filters=columns*rows
+for i in range(1, n_filters+1):
+    f=filters[:, :, :, i-1]
+    fig1 = plt.subplot(rows, columns, i)
+    fig1.set_xticks([])
+    fig1.set_yticks([])
+    plt.imshow(f[:, :, 0], cmap="gray")
+plt.show()
+
+#Analyzing the filter outputs
+#For that, we will define a truncated architecture to study the filter outputs
+conv_layer_index=[0, 2, 4, 5, 6, 7, 9, 10, 11, 12]  # you can choose any convolution layers, you can find the indices of them from the layer array
+outputs = [model.layers[i].output for i in conv_layer_index]
+model_short = Model(inputs=model.inputs, outputs=outputs)
+print(model_short.summary()) #This will show the complete analysis ofyour architecture
+
+#Input image of size = 448*448
+from keras.preprocessing.image import load_img, img_to_array
+img = load_img("test2.png")
+
+img = img_to_array(img)
+img = np.expand_dims(img, axis=0)
+
+feature_output = model_short.predict(img)
+
+columns = 8
+rows = 8
+for ftr in feature_output:
+    fig=plt.figure(figsize=(12,12))
+    for i in range(1, columns*rows +1):
+        fig=plt.subplot(rows, columns, i)
+        fig.set_xticks([])
+        fig.set_yticks([])
+        plt.imshow(ftr[0, :, :, i-1], cmap="gray")
+    plt.show()
